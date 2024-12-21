aec83855a65b41abc49af88d1bba54afb540af91
Atr0fy <2006lucasns@gmail.com>
2024-12-20 21:26:34 -0300

    Correção do header mobile

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef  } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import darkLogo from '../../img/dark.svg';
 import lightLogo from '../../img/light.svg';
 import gsap from 'gsap';
@@ -35,11 +35,11 @@ const Header = () => {
             return () => clearTimeout(timer);
         }
     }, []);
-    useEffect(() =>{
-        if (subMenuRef.current){
-            subMenuRef.current.style.display = isMenuVisible ? 'grid' : 'none' 
+    useEffect(() => {
+        if (subMenuRef.current) {
+            subMenuRef.current.style.display = isMenuVisible ? 'grid' : 'none'
         }
-        
+
         if (menuSvgRef.current) {
             gsap.to(menuSvgRef.current, {
                 attr: { d: isMenuVisible ? openPath : closedPath },
@@ -52,7 +52,7 @@ const Header = () => {
             if (isMenuVisible) {
                 const height = subMenuRef.current.scrollHeight;
                 gsap.to(subMenuRef.current, {
-                    height: height, 
+                    height: height,
                     duration: 0.5,
                     ease: "power4.inOut",
                 });
@@ -89,8 +89,8 @@ const Header = () => {
                 )
 
         }
-    }, [isDarkTheme]); 
-    
+    }, [isDarkTheme]);
+
     useEffect(() => {
         const handleScroll = () => {
             const header = document.querySelector('header');
@@ -118,26 +118,35 @@ const Header = () => {
                 <Link to="/" state={{ scrollTo: 'projetosId' }} className='aHeader'>Projetos</Link>
                 <Link to="/" state={{ scrollTo: 'habilidadesId' }} className='aHeader'>Habilidades</Link>
                 <Link to="/" state={{ scrollTo: 'contatosId' }} className='contactBtn'>Contato</Link>
-                <img ref={imgTheme}
-                    src={themeImgSrc}
-                    className="imgTheme"
-                    alt={isDarkTheme ? 'Tema Claro' : 'Tema Escuro'}
-                    onClick={toggleTheme}
-                />
-                <div className="menu" onClick={toggleMenu}>
-                    <svg viewBox="4 4 16 16" fill="var(--cor-preta)" xmlns="http://www.w3.org/2000/svg" className='menuSvg'>
-                        <path ref={menuSvgRef} d={closedPath} stroke="var(--cor-preta)" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
-                    </svg>
-                    <div ref={subMenuRef} className="sub-menu">
-                        <a href="#homeId">Inicio</a>
-                        <a href="#sobreId">Sobre</a>
-                        <a href="#projetosId">Projetos</a>
-                        <a href="#habilidadesId">Habilidades</a>
-                        <a href="#contatosId" className="contactBtn">Contato</a>
-                    </div>
-                </div>
-
             </nav>
+            <img ref={imgTheme}
+                src={themeImgSrc}
+                className="imgTheme"
+                alt={isDarkTheme ? 'Tema Claro' : 'Tema Escuro'}
+                onClick={toggleTheme}
+            />
+            <div className='headerMobile'>
+                <div className='headerMobileIcons'>
+                    <img ref={imgTheme}
+                        src={themeImgSrc}
+                        className="imgTheme imgThemeMobile"
+                        alt={isDarkTheme ? 'Tema Claro' : 'Tema Escuro'}
+                        onClick={toggleTheme}
+                    />
+                    <div className="menu" onClick={toggleMenu}>
+                        <svg viewBox="4 4 16 16" fill="var(--cor-preta)" xmlns="http://www.w3.org/2000/svg" className='menuSvg'>
+                            <path ref={menuSvgRef} d={closedPath} stroke="var(--cor-preta)" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+                        </svg>
+                    </div>
+                </div> 
+                <div ref={subMenuRef} className="sub-menu">
+                    <Link to="/" state={{ scrollTo: 'homeId' }} >Inicio</Link>
+                    <Link to="/" state={{ scrollTo: 'sobreId' }} >Sobre</Link>
+                    <Link to="/" state={{ scrollTo: 'projetosId' }} >Projetos</Link>
+                    <Link to="/" state={{ scrollTo: 'habilidadesId' }} >Habilidades</Link>
+                    <Link to="/" state={{ scrollTo: 'contatosId' }} className='contactBtn'>Contato</Link>
+                </div>
+            </div>
         </header>
     );
 };
