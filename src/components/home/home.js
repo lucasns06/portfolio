c09426a68e8a70e0d2bc85ca53a28650f0abec88
Atr0fy <2006lucasns@gmail.com>
2024-12-25 21:46:11 -0300

    adição de particulas com particlesjs

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -1,14 +1,15 @@
 import { useLayoutEffect, useRef } from "react";
 import React from "react";
-import homeImage from '../../img/User Interface.gif';
+import homeImage from '../../img/perfil.png';
 import { gsap } from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger'
 import { TextPlugin } from "gsap/TextPlugin";
 import { Link } from 'react-router-dom';
+import ParticlesComponent from '../particles'
+import { ReactTyped } from "react-typed";
 import './home.css'
 import '../../App.css';
 const Home = () => {
-
     const saberBtn = useRef();
     const homeImg = useRef();
     useLayoutEffect(() => {
@@ -36,15 +37,14 @@ const Home = () => {
                 duration: 2
             });
 
-        gsap.fromTo(animTextHome, {
-            opacity: 0,
-            x: -20,
-        },
-            {
+        setTimeout(() => {
+            gsap.to(animTextHome, {
                 x: 0,
                 opacity: 1,
                 duration: 4
             });
+        }, 7000)
+
         gsap.fromTo(homeP, {
             opacity: 0,
         }, {
@@ -68,7 +68,6 @@ const Home = () => {
 
         const homeImage = homeImg.current;
         homeImage.style.opacity = 0;
-
         setTimeout(() => {
             gsap.to(homeImage,
                 {
@@ -76,21 +75,32 @@ const Home = () => {
                     duration: 2,
                 })
         }, 6000)
-
     }, []);
 
     return (
         <div className="homeMain" id="homeId">
             <div className="home">
                 <div className="home-texto">
-                    <h1 className="nomeHome textAnim2">Olá, eu sou <span className="span">Lucas</span> <br />
-                        <p className="animTextHome">Desenvolvedor de Software</p>
+                    <h1 className="nomeHome textAnim2">Olá &#128075;, eu sou <span className="span">Lucas</span> <br />
+                        <p className="animTextHome">
+                            <ReactTyped
+                                strings={[
+                                    "Programador Full Stack",
+                                    "Designer",
+                                    "Estudante"
+                                ]}
+                                typeSpeed={40}
+                                backSpeed={50}
+                                loop >
+                            </ReactTyped>
+                        </p>
                     </h1>
                     <p className="textWriting"></p>  <br /> <br />
                     <Link to="/" state={{ scrollTo: 'sobreId' }} ><button className='projetosBtn versiteBtn sombra' ref={saberBtn}>Saber Mais</button></Link>
                 </div>
                 <img src={homeImage} className="homeImg sombra" alt="HomeImg" ref={homeImg} />
             </div>
+            <ParticlesComponent id="tsparticles" />
             <p>“A persistência é o caminho do êxito” <strong>--Charles Chaplin</strong></p>
         </div>
     );
