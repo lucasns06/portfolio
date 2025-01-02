e6f13770ed446f9111e66139f62541de492cd09a
Atr0fy <2006lucasns@gmail.com>
2025-01-01 22:14:23 -0300

    mudança no sobre

--- src/components/sobre/sobre.js
+++ src/components/sobre/sobre.js
@@ -27,103 +27,54 @@ const Sobre = () => {
                 }
             );
         });
+        const sobreTextos = document.querySelectorAll('.sobre-texto p');
+        const sobreContainerImg = document.querySelectorAll('.sobreContainerImg');
+        setTimeout(() => {
+            gsap.to(
+                sobreContainerImg,
+                {
+                    opacity: 1,
+                    duration: 2
+                })
+            if (sobreTextos) {
+                gsap.fromTo(
+                    sobreTextos,
+                    { y: 80, },
+                    {
+                        scrollTrigger: {
+                            trigger: sobreTextos,
+                            toggleActions: "play none none none"
+                        },
+                        y: 0,
+                        opacity: 1,
+                        duration: 1,
+                        stagger: 0.8
+                    },
+                );
+            }
+        }, 1000)
 
-        const timeline = gsap.timeline({
-            scrollTrigger: {
-                trigger: ".sobre",
-                start: "top center",
-                toggleActions: "play none none none",
-            },
-        });
 
-        const cards = document.querySelectorAll('.card');
-        const firstThreeCards = Array.from(cards).slice(0, 3);
-        const lastThreeCards = Array.from(cards).slice(3);
 
-        timeline.fromTo(
-            texts,
-            { y: 100, opacity: 0 },
-            { y: 0, opacity: 1, duration: 0.5 }
-        );
-
-        timeline.to(
-            '.textWriting2',
-            {
-                text: "Lucas Nascimento",
-                duration: 1,
-            },
-        );
-        timeline.fromTo(
-            firstThreeCards,
-            { y: 100, opacity: 0 },
-            { y: 0, opacity: 1, duration: 0.5, stagger: 0.2 },
-        );
-
-        timeline.fromTo(
-            lastThreeCards,
-            { y: 100, opacity: 0 },
-            { y: 0, opacity: 1, duration: 0.5, stagger: 0.2 },
-
-        );
 
     }, []);
 
     return (
         <div className="sobre" id="sobreId">
-            <div className="sobreContainer-meio sobre-mobile">
-                <h1 className="QuemSouP textAnim">Sobre <span className="span">mim</span></h1>
-                <div className="sobreContainerImg sombra">
-                    <img src={perfilFoto} alt="Perfil" />
-                </div> <br />
-                <p className="textWriting2"></p><br /> <br />
-            </div>
+            <h1 className="QuemSouP textAnim">Sobre <span className="span">mim</span></h1>
             <div className="sobreContainer">
-                <div className="cards">
-                    <div className="card">
-                        <p>
-                            Desde jovem, sempre fui apaixonado por desenho, música e tecnologia
-                        </p>
-                    </div>
-                    <div className="card">
-                        <p>
-                            Em 2024, iniciei o curso de Técnico em Desenvolvimento de Sistemas.
-                        </p>
-                    </div>
-                    <div className="card">
-                        <p>
-                            Eu Também fiz Técnico em Administração integrado ao Ensino Médio
-                        </p>
-
-                    </div>
-                </div>
-                <div className="sobreContainer-meio">
-                    <h1 className="QuemSouP textAnim">Sobre <span className="span">mim</span></h1>
+                <div>
                     <div className="sobreContainerImg">
                         <img src={perfilFoto} alt="Perfil" className="sombra" />
-                    </div> <br />
-                    <p className="textWriting2"></p><br /> <br />
-                    <Link to="/" state={{ scrollTo: 'projetosId' }} className="projetosBtn versiteBtn sombra">Ver Projetos</Link>
-                </div>
-                <div className="cards">
-                    <div className="card">
-                        <p>
-                            Tenho hobbies como desenhar e tocar violão.
-                        </p>
                     </div>
-                    <div className="card">
-                        <p>
-                            Atualmente, estou em busca de novas oportunidades para aplicar meus conhecimentos e continuar evoluindo na área.
-                        </p>
-                    </div>
-                    <div className="card">
-                        <p>
-                            Veja os meus projetos! :D
-                        </p>
-                    </div> 
                 </div>
-            </div> <br />
-            <Link to="/" state={{ scrollTo: 'projetosId' }} className="projetosBtn versiteBtn sombra btn-mobile">Ver Projetos</Link> 
-        </div> 
+                <div className="sobre-texto">
+                    <p>Me chamo Lucas Nascimento, e eu sempre gostei de tecnologia.</p> <br />
+                    <p>Entrei no mundo da programação com técnico em desenvolvimento de sistemas.</p> <br />
+                    <p>Atualmente sou Full Stack e estou procurando oportunidade na área.</p>
+                </div>
+            </div>
+        </div>
     );
 }
 
