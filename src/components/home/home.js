ebf67213efabce1cefcd5e4034a1a0038d59d057
lucasns06 <lucasnascimento.carreira@gmail.com>
2025-01-29 22:00:10 -0300

    🔨 timeline na animacao do home

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -1,95 +1,88 @@
 import { useEffect, useLayoutEffect, useRef, useState } from "react";
 import React from "react";
-import { gsap } from 'gsap';
-import { ScrollTrigger } from 'gsap/ScrollTrigger'
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 import { TextPlugin } from "gsap/TextPlugin";
-import { Link } from 'react-router-dom';
-import ParticlesComponent from '../particles'
+import { Link } from "react-router-dom";
+import ParticlesComponent from "../particles";
 import { ReactTyped } from "react-typed";
-import './home.css'
-import '../../App.css';
-const Home = ({isDarkTheme}) => {
-    const saberBtn = useRef();
+import "./home.css";
+import "../../App.css";
+const Home = ({ isDarkTheme }) => {
+  const saberBtn = useRef();
 
-    useLayoutEffect(() => {
-        const animTextHome = document.querySelector('.animTextHome')
-        const textHome = document.querySelector('.textAnim2')
-        const homeP = document.querySelector('.homeMain > p')
+  useLayoutEffect(() => {
+    gsap.registerPlugin(TextPlugin);
+    gsap.registerPlugin(ScrollTrigger);
 
-        gsap.registerPlugin(TextPlugin);
-        gsap.registerPlugin(ScrollTrigger);
-        setTimeout(() => {
-            gsap.to(".textWriting", {
-                duration: 4,
-                text: "Sou um Desenvolvedor FullStack Júnior. <br/> Estou procurando uma oportunidade de trabalho e aprendizado."
-            });
-        }, 1500)
+    const animTextHome = document.querySelector(".animTextHome");
+    const textHome = document.querySelector(".textAnim2");
+    const saberBotao = saberBtn.current;
 
+    var tl = gsap.timeline();
 
-        gsap.fromTo(textHome, {
-            opacity: 0,
-            x: -0,
-        },
-            {
-                opacity: 1,
-                y: 0,
-                duration: 2
-            });
+    tl.fromTo(
+      textHome,
+      {
+        opacity: 0,
+        y: -40,
+      },
+      {
+        opacity: 1,
+        y: 0,
+        duration: 2,
+      }
+    )
+      .to(".textWriting", {
+        duration: 4,
+        text: "Sou um Desenvolvedor FullStack Júnior. <br/> Estou procurando uma oportunidade de trabalho e aprendizado.",
+      })
+      .to(saberBotao, {
+        opacity: 1,
+        display: "inline",
+        duration: 2,
+      })
+      .to(animTextHome, {
+        x: 0,
+        opacity: 1,
+        duration: 4,
+      });
+  }, []);
 
-        setTimeout(() => {
-            gsap.to(animTextHome, {
-                x: 0,
-                opacity: 1,
-                duration: 4
-            });
-        }, 5000)
-
-        gsap.fromTo(homeP, {
-            opacity: 0,
-        }, {
-            opacity: 1,
-            duration: 2,
-            scrollTrigger: {
-                trigger: homeP,
-                start: 'top 50%',
-            }
-        })
-        const saberBotao = saberBtn.current;
-        setTimeout(() => {
-            gsap.to(saberBotao,
-                {
-                    opacity: 1,
-                    display: 'inline',
-                    duration: 2,
-                })
-        }, 5500)
-
-    }, []);
-
-    return (
-        <div className={`homeMain ${isDarkTheme ? "backgroundBlack" : "backgroundWhite"}`} id="homeId">
-            <div className="home">
-                <div className="home-texto">
-                    <h1 className="nomeHome textAnim2">Olá, eu sou <span className="span">Lucas</span> <br />
-                        <p className="animTextHome">
-                            <ReactTyped
-                                strings={[
-                                    "Desenvolvedor Full Stack",
-                                    "Desenvolvedor Júnior"
-                                ]}
-                                typeSpeed={40}
-                                backSpeed={50}
-                                loop >
-                            </ReactTyped>
-                        </p>
-                    </h1>
-                    <p className="textWriting"></p>  <br /> <br />
-                    <Link to="/" state={{ scrollTo: 'sobreId' }} ><button className='projetosBtn versiteBtn sombra saberBtn' ref={saberBtn}>Saber Mais</button></Link>
-                </div>
-            </div>
-            <ParticlesComponent id="tsparticles" />
+  return (
+    <div
+      className={`homeMain ${
+        isDarkTheme ? "backgroundBlack" : "backgroundWhite"
+      }`}
+      id="homeId"
+    >
+      <div className="home">
+        <div className="home-texto">
+          <h1 className="nomeHome textAnim2">
+            Olá, eu sou <span className="span">Lucas</span> <br />
+            <p className="animTextHome">
+              <ReactTyped
+                strings={["Desenvolvedor Full Stack", "Desenvolvedor Júnior"]}
+                typeSpeed={40}
+                backSpeed={50}
+                loop
+              ></ReactTyped>
+            </p>
+          </h1>
+          <p className="textWriting"></p> <br /> <br />
+          <Link to="/" state={{ scrollTo: "sobreId" }}>
+            <button
+              className="projetosBtn versiteBtn sombra saberBtn"
+              ref={saberBtn}
+            >
+              Saber Mais
+            </button>
+          </Link>
         </div>
-    );
-}
+      </div>
+      <ParticlesComponent id="tsparticles" />
+    </div>
+  );
+};
 
 export default Home;
